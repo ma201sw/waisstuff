0fcc8d5984056002f7e50ebbaa7d763482b93b03
ma201sw <shauwong@hotmail.com>
2016-10-22 01:32:40 +0100

    c

--- gs-maven/src/main/java/hello/Application.java
+++ gs-maven/src/main/java/hello/Application.java
@@ -17,7 +17,7 @@ public class Application  {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        System.out.println("Lety's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
