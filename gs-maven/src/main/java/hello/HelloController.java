005748e7184bd369985bbeb75c6f1b7f90ff63e2
ma201sw <shauwong@hotmail.com>
2016-10-22 22:28:45 +0100

    c

--- gs-maven/src/main/java/hello/HelloController.java
+++ gs-maven/src/main/java/hello/HelloController.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.HashMap;
 
 import org.json.JSONObject;
@@ -10,7 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class HelloController {
@@ -39,6 +41,7 @@ public class HelloController {
     }
     
     @RequestMapping("/greeting")
+    @ResponseStatus(HttpStatus.OK)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(1,name);
     }
